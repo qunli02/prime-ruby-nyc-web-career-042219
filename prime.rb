82b5fb80da9141e690ee55f0af2e735a79f0b5ff
Qun Li Huang <qunli02@gmail.com>
2019-03-31 23:21:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,19 +2,21 @@
 def prime?(x)
   number = x - 1
   until number == 1
-    if x == 2
-      return true
-      break
-    elsif x > 2 
-      return false
-      break
-    elsif x % number == 0
-      return true
-      break
-    else
-      number -= 1
-    end
-    
+    break if x < 2 || x == 2 
   end
     
 end
+
+
+#    if x == 2
+#      return true
+#     break
+#    elsif x > 2 
+#      return false
+#      break
+#    elsif x % number == 0
+#      return true
+#      break
+#    else
+#      number -= 1
+#    end
